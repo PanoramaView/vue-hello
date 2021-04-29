13425981cf774f1caa44a0152b0ba2e32289677c
PanoramaView <zhou-1997@hotmail.it>
2021-04-29 22:48:46 +0200

    ok

---
+++ myscripts.js
@@ -0,0 +1,11 @@
+// Inizializziamo Vue
+const app = new Vue(
+    {
+        el: '#vueContainer',
+        data: {
+            message: 'Messaggio dentro un h1'
+        },
+        methods: {
+        }
+    }
+);
