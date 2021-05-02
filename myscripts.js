60e809b56811885da3a522b7af2641d64a966a8c
PanoramaView <zhou-1997@hotmail.it>
2021-05-02 17:48:27 +0200

    added img

--- myscripts.js
+++ myscripts.js
@@ -3,7 +3,8 @@ const app = new Vue(
     {
         el: '#vueContainer',
         data: {
-            message: 'Messaggio dentro un h1'
+            message: 'Messaggio dentro un h1',
+            imgurl: "https://art.ngfiles.com/images/1570000/1570772_diives_baozi-take-me-home-with-you.png?f1609543199"
         },
         methods: {
         }
